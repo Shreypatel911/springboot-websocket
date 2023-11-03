a30e59edc1d3c371359e9dd2b1fcf4f0841cc515
Shreypatel911 <77115893+Shreypatel911@users.noreply.github.com>
2023-11-02 20:51:53 -0700

    first commit

---
+++ src/main/java/com/example/springwebsocketapp/SpringWebsocketAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.springwebsocketapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebsocketAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebsocketAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springwebsocketapp/SpringWebsocketAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springwebsocketapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebsocketAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
